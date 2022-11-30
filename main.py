c49a3a742dc6b03ccf820f9cfd83e5878e24e46a
iesfernandoiii <56558316+iesfernandoiii@users.noreply.github.com>
2022-11-30 19:48:34 +0100

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
+def on_button_pressed_a():
+    if reloj_en_hora:
+        RTC_DS1307.set_time(RTC_DS1307.TimeType.MINUTE,
+            RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE) + 1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if reloj_en_hora:
+        RTC_DS1307.set_time(RTC_DS1307.TimeType.MINUTE,
+            RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE) - 1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def mostrar_datos_display():
+    OLED.clear()
+    OLED.write_string("Luz:")
+    OLED.write_num(Environment.read_light_intensity(AnalogPin.P1))
+    OLED.new_line()
+    OLED.write_string("Temperatura (Cº):")
+    OLED.write_num(Environment.octopus_BME280(Environment.BME280_state.BME280_TEMPERATURE_C))
+    OLED.new_line()
+    OLED.write_string("Humedad:")
+    OLED.write_num(Environment.octopus_BME280(Environment.BME280_state.BME280_HUMIDITY))
+reloj_en_hora = False
+reloj_en_hora = False
+if not (reloj_en_hora):
+    reloj_en_hora = True
+    RTC_DS1307.date_time(2022, 11, 30, 19, 15, 0)
+    basic.show_string("" + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.HOUR)) + ":" + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE)))
+OLED.init(128, 64)
+ESP8266_IoT.init_wifi(SerialPin.P8, SerialPin.P12, BaudRate.BAUD_RATE115200)
+ESP8266_IoT.connect_wifi("LBWifi", "LosBanyos")
+
 def on_forever():
-    pass
+    basic.show_string("" + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.HOUR)) + ":" + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE)))
+    mostrar_datos_display()
+    if ESP8266_IoT.wifi_state(True):
+        ESP8266_IoT.connect_thing_speak()
+        if ESP8266_IoT.thing_speak_state(True):
+            ESP8266_IoT.set_data("F0GCA2MBNTD7YHQ3",
+                Environment.octopus_BME280(Environment.BME280_state.BME280_TEMPERATURE_C),
+                Environment.octopus_BME280(Environment.BME280_state.BME280_HUMIDITY),
+                Environment.read_light_intensity(AnalogPin.P1))
+    basic.pause(60000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    if (reloj_en_hora) {
-        RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + 1)
-    }
+    RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + 1)
 })
 input.onButtonPressed(Button.B, function () {
-    if (reloj_en_hora) {
-        RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) - 1)
-    }
+    RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) - 1)
 })
 function mostrar_datos_display () {
     OLED.clear()
@@ -19,23 +15,36 @@ function mostrar_datos_display () {
     OLED.writeString("Humedad:")
     OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
 }
-let reloj_en_hora = false
-reloj_en_hora = false
-if (!(reloj_en_hora)) {
-    reloj_en_hora = true
-    RTC_DS1307.DateTime(
-    2022,
-    11,
-    30,
-    19,
-    15,
-    0
-    )
-    basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE))
-}
+basic.showString("Poniendo en hora")
+RTC_DS1307.DateTime(
+2022,
+11,
+30,
+19,
+15,
+0
+)
+basic.showString("Hora fijada")
+basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE))
+basic.pause(500)
+basic.showString("Iniciando OLED")
 OLED.init(128, 64)
+OLED.clear()
+OLED.drawLoading(25)
+basic.showString("OLED iniciado")
+basic.pause(200)
+basic.showString("Iniciando Wifi")
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("LBWifi", "LosBanyos")
+basic.pause(500)
+if (ESP8266_IoT.wifiState(true)) {
+    basic.showString("Wifi conectado")
+    OLED.clear()
+    OLED.drawLoading(50)
+} else {
+    basic.showString("Wifi NO")
+}
+basic.pause(500)
 basic.forever(function () {
     basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE))
     mostrar_datos_display()
