cc453abe9b40386b4d9afccce5e04bf12f76f7b6
iesfernandoiii <56558316+iesfernandoiii@users.noreply.github.com>
2022-11-30 19:21:52 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    if (reloj_en_hora) {
+        RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + 1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (reloj_en_hora) {
+        RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) - 1)
+    }
+})
 function mostrar_datos_display () {
     OLED.clear()
     OLED.writeString("Luz:")
@@ -9,10 +19,25 @@ function mostrar_datos_display () {
     OLED.writeString("Humedad:")
     OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
 }
+let reloj_en_hora = false
+reloj_en_hora = false
+if (!(reloj_en_hora)) {
+    reloj_en_hora = true
+    RTC_DS1307.DateTime(
+    2022,
+    11,
+    30,
+    19,
+    15,
+    0
+    )
+    basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE))
+}
 OLED.init(128, 64)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("LBWifi", "LosBanyos")
 basic.forever(function () {
+    basic.showString("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE))
     mostrar_datos_display()
     if (ESP8266_IoT.wifiState(true)) {
         ESP8266_IoT.connectThingSpeak()
